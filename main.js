7a641184d1f2038f53f424fcb528ded98cb2eab6
Chijioke <wisestcodes@gmail.com>
2023-06-09 20:49:30 -0400

    when button is clicked

---
+++ main.js
@@ -0,0 +1,33 @@
+let apiQuotes = [];
+const quoteText = document.getElementById("quote");
+const quoteAuthor = document.getElementById("author");
+const newQuoteButton = document.getElementById("new_quote");
+
+// get new quote
+function newQuote() {
+  // pick random quote from api quotes array
+  const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+  return quote;
+}
+
+function setTextAndAuthor(newQuote) {
+  quoteText.textContent = newQuote.text;
+  quoteAuthor.textContent = newQuote.author;
+}
+
+// get quotes from API
+const getQuotes = async () => {
+  const apiUrl = "https://type.fit/api/quotes";
+  try {
+    const response = await fetch(apiUrl);
+    apiQuotes = await response.json();
+    setTextAndAuthor(newQuote());
+  } catch (error) {}
+};
+
+// On load
+getQuotes();
+
+newQuoteButton.addEventListener("click", () => {
+  setTextAndAuthor(newQuote());
+});
